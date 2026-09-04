6f2f98ef3b28c1dea47e8f5b496f9c2a1e4705f4
Miguel Andy Contreras Esparza <105465066+Diomastrio@users.noreply.github.com>
2026-09-03 18:34:44 -0600

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+productExceptSelf(nums) {
+    const n = nums.length;
+    const result = new Array(n).fill(1);
+// Pass 1: fill result[i] with prefix product (product of everything before i)
+/*
+[1,2,4,6]
+result 1
+prefix = 2 * 1
+suffix = 2 * 6
+
+*/
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        result[i] = prefix;
+        prefix = prefix * nums[i];
+    }
+// Pass 2: multiply in the suffix product (product of everything after i)
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] = result[i] * suffix;
+        suffix = suffix * nums[i];
+    }
+
+    return result;
+}
+}
